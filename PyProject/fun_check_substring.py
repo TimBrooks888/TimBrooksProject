211abcd62f6ecb6e53da9bbc58e89dbdd0d0ef74
TimBrooks888 <167555008+TimBrooks888@users.noreply.github.com>
2024-07-08 18:37:12 +0900

    Create fun_check_substring.py

---
+++ PyProject/fun_check_substring.py
@@ -0,0 +1,6 @@
+# fun_check_substring.py
+def fun_check_substring(string, substring):
+    return substring in string
+
+print(fun_check_substring("hello world", "world"))
+print(fun_check_substring("hello world", "python"))
