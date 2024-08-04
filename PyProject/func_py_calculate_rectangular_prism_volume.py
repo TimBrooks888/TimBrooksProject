93914bc878a452c4225bb9362b21c544cc7f95b9
TimBrooks888 <167555008+TimBrooks888@users.noreply.github.com>
2024-08-04 14:48:14 +0900

    Create func_py_calculate_rectangular_prism_volume.py

---
+++ PyProject/func_py_calculate_rectangular_prism_volume.py
@@ -0,0 +1,5 @@
+# func_py_calculate_rectangular_prism_volume.py
+def func_py_calculate_rectangular_prism_volume(length, width, height):
+    return length * width * height
+
+print(func_py_calculate_rectangular_prism_volume(3, 4, 5))
