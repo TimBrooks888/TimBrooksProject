ca77761f0fc1efbb5a70d41f1309ce04ce33d30f
TimBrooks888 <167555008+TimBrooks888@users.noreply.github.com>
2024-08-16 21:37:01 +0900

    Create func_py_calculate_right_prism_surface_area.py

---
+++ PyProject/func_py_calculate_right_prism_surface_area.py
@@ -0,0 +1,5 @@
+# func_py_calculate_right_prism_surface_area.py
+def func_py_calculate_right_prism_surface_area(base_perimeter, base_area, height):
+    return (2 * base_area) + (base_perimeter * height)
+
+print(func_py_calculate_right_prism_surface_area(18, 20, 10))
