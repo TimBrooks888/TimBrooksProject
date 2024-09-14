3ede0472071f2ab02476bf83c5ec2e6394da4306
TimBrooks888 <167555008+TimBrooks888@users.noreply.github.com>
2024-09-14 23:26:33 +0900

    Create func_py_sort_strings_alphabetically.py

---
+++ PyProject/func_py_sort_strings_alphabetically.py
@@ -0,0 +1,5 @@
+# func_py_sort_strings_alphabetically.py
+def func_py_sort_strings_alphabetically(lst):
+    return sorted(lst)
+
+print(func_py_sort_strings_alphabetically(["banana", "apple", "orange"]))
