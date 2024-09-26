edfdf30380df879fc2b71a767b3a444d73581def
TimBrooks888 <167555008+TimBrooks888@users.noreply.github.com>
2024-09-26 18:19:29 +0900

    Create func_py_generate_random_number.py

---
+++ PyProject/func_py_generate_random_number.py
@@ -0,0 +1,7 @@
+# func_py_generate_random_number.py
+import random
+
+def func_py_generate_random_number(start, end):
+    return random.randint(start, end)
+
+print(func_py_generate_random_number(1, 100))
